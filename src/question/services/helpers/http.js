2bb5e667005dc15e8c772e7e536798f3877dcf5e
HQRDa11 <thomasaserfroc@yahoo.fr>
2022-03-19 01:31:17 +0100

    Update http.js

--- src/question/services/helpers/http.js
+++ src/question/services/helpers/http.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const customAxios = axios.create({
-  baseURL: 'http://localhost:8080/api/',
+  baseURL: 'http://localhost:8080/cdamassy2021/api/',
 
   headers: {
     'Content-Type': 'application/json',
-    "Access-Control-Allow-Origin": "*",
+    'Access-Control-Allow-Origin': '*',
   },
 })
 
