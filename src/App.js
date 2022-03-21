da61fecf32749c6960abe4f8b670c299f10b202d
Christian MOUSTACHE <christian.moustache@gmail.com>
2022-03-21 15:48:35 +0100

    "rajout composant Efgs"

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { Provider } from 'react-redux';
 import QuestionWorkspace from './components/questions/QuestionWorkspace';
 import Canaux from './components/canaux/Canaux';
 import Membres from './components/membres/Membres';
+import Efgs from './components/efgs/Efgs';
 import { store } from './redux/store';
 import { QuestionsProvider } from './contexts/questions.context';
 
@@ -35,22 +36,25 @@ function App() {
           </nav>
           
         <Switch>
+
           <Route path="/questions">
             <QuestionsProvider>
               <QuestionWorkspace/>
             </QuestionsProvider>
           </Route>
 
-
           <Route path="/canaux">
               <Canaux/>
           </Route>
+
           <Route path="/membres">
               <Membres/>
           </Route>
-          {/* <Route path="/efg">
-              <MyComponent/>
-          </Route> */}
+
+          <Route path="/efg">
+              <Efgs/>
+          </Route>
+
         </Switch>
       </div>
     </Router>
---
+++ src/components/efgs/Efgs.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+//import {Link} from "react-router-dom";
+
+const Efgs = () => {
+    return (
+        <div>
+            <p><h1>Liste Efg avec le formateur </h1></p>
+            <p><h2> Liste GroupeEfg par exercice</h2></p>
+            <p><h3>Liste MembreGroupeEfg par groupe</h3></p>
+        </div>
+    )
+}
+
+export default Efgs;
