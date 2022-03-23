f3023226493ad36c50bcdaffde40c186925ac576
ikescd <ikescd@gmail.com>
2022-03-23 11:54:09 +0100

    ajoute lien d'accès aux exercices sur canal.js

--- src/components/canaux/Canal.js
+++ src/components/canaux/Canal.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react'
-import {Link} from "react-router-dom";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-const Canal = () =>{
-    
-        return (
-            <div>
-                <h1>Canal</h1>
-                <Link to="/membres">Nom du Canal</Link>
-            </div>
-        )
+const Canal = () => {
+	return (
+		<div>
+			<h1>Canal</h1>
+			<Link to='/membres'>Nom du Canal</Link>
+			<br />
+			<Link to='/efgs'>Exercices à faire en groupe</Link>
+		</div>
+	);
+};
 
-        }
-  
 export default Canal;
