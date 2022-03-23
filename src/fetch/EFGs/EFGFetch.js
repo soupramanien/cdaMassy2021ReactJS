223e9371ea14011d05af92496abb43bd0d8340e4
Florian Langlois <florianlnglois@gmail.com>
2022-03-23 10:33:56 +0100

    mise à jour url des fetchs efg

--- src/fetch/EFGs/EFGFetch.js
+++ src/fetch/EFGs/EFGFetch.js
@@ -6,7 +6,7 @@
 const url = '';
 export default class EFGServices {
 	static getAllEFGs(props, idCanal) {
-		fetch(`http://localhost:8080/cdamassy2021/canaux/${idCanal}/EFGs/`)
+		fetch(`http://localhost:8080/cdamassy2021/api/${idCanal}/EFGs`)
 			.then((response) => response.json())
 			.then((data) => {
 				props(data);
@@ -15,7 +15,7 @@ export default class EFGServices {
 	}
 
 	static getEFG(props, idEFG) {
-		fetch(`http://localhost:8080/cdamassy2021/canaux/1/EFGs/${idEFG}`)
+		fetch(`http://localhost:8080/cdamassy2021/api/1/EFGs/${idEFG}`)
 			.then((response) => response.json())
 			.then((data) => {
 				props(data);
