61f9f93b2cd695e1b88ade115c97d557fc88a682
ikescd <ikescd@gmail.com>
2022-03-24 14:33:42 +0100

    bloque la création d'un exercice si élèves insuffisants + affiche pas d'exo si pas d'exo

--- src/pages/EFGs/EFGList.js
+++ src/pages/EFGs/EFGList.js
@@ -14,25 +14,37 @@ function EFGList() {
 		EFGServices.getNombreMembres(setMembers, canalId);
 	}, [canalId]);
 
+	console.log(members);
 	return (
 		<div id='efg-container'>
-			<h1>Liste des exercices à faire en groupe</h1>
-			<table className='tab-efg'>
-				<thead>
-					<tr>
-						<th>N°</th>
-						<th>Intitulé</th>
-						<th>Groupes</th>
-						<th>Lien de l'exercice</th>
-					</tr>
-				</thead>
-				<tbody>
-					{efgs.map((efg) => (
-						<EFGCard efg={efg} key={efg.idEfg} isDisabled={false} />
-					))}
-				</tbody>
-			</table>
+			{efgs.length >= 1 && (
+				<>
+					<h1>Liste des exercices à faire en groupe</h1>
+					<table className='tab-efg'>
+						<thead>
+							<tr>
+								<th>N°</th>
+								<th>Intitulé</th>
+								<th>Groupes</th>
+								<th>Lien de l'exercice</th>
+							</tr>
+						</thead>
+						<tbody>
+							{efgs.map((efg) => (
+								<EFGCard efg={efg} key={efg.idEfg} isDisabled={false} />
+							))}
+						</tbody>
+					</table>
+				</>
+			)}
+			{efgs.length === 0 && <p>Il n'y a aucun exercice dans ce canal.</p>}
 
+			{(members < 5 || members === undefined || members === null) && (
+				<p>
+					Veuillez rajouter des élèves dans votre canal pour pouvoir créer un
+					exercice.
+				</p>
+			)}
 			{members >= 5 && <EFGButton props={'new'} />}
 		</div>
 	);
