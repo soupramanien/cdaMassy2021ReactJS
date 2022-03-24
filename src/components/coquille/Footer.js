9e1d610560fc40777271a2fa7d000932f27d0000
kamal9401 <kamalknina@hotmail.fr>
2022-03-24 18:30:01 +0100

    delete  file don't used

--- src/components/coquille/Footer.js
+++ src/components/coquille/Footer.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Footer extends Component {
-    render() {
-        return (
-           <div>
-  <footer className="main-footer">
-    <strong>Copyright © 2022 <a href="http://dev.greta91.coö">Cda DEV</a>.</strong>
-    All rights reserved.
-    <div className="float-right d-none d-sm-inline-block">
-      <b>Version</b> 0.0.1
-    </div>
-  </footer>
-</div>
-
-        )
-    }
+	render() {
+		return (
+			<div>
+				<footer className="main-footer">
+					<strong>
+						Copyright © 2022 <a href="http://dev.greta91.com">Cda DEV</a>.
+					</strong>
+					All rights reserved.
+					<div className="float-right d-none d-sm-inline-block">
+						<b>Version</b> 0.0.1
+					</div>
+				</footer>
+			</div>
+		);
+	}
 }
--- src/components/coquille/Menu.js
+++ src/components/coquille/Menu.js
@@ -1,76 +1,89 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Menu extends Component {
-    render() {
-        return (
-          <div>
-  <aside className="main-sidebar sidebar-dark-primary elevation-4">
-    {/* Brand Logo */}
-    <a href="'#'" className="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="Cda Massy 2022" className="brand-image img-circle elevation-3" style={{opacity: '.8'}} />
-      <span className="brand-text font-weight-light">Cda Massy 2022</span>
-    </a>
-    {/* Sidebar */}
-    <div className="sidebar">
-      {/* Sidebar user panel (optional) */}
-      <div className="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div className="image">
-          <img src="dist/img/user2-160x160.jpg" className="img-circle elevation-2" alt="User Image" />
-        </div>
-        <div className="info">
-          <a href="#" className="d-block">Kamal knina</a>
-        </div>
-      </div>
-      {/* Sidebar Menu */}
-      <nav className="mt-2">
-        <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          {/* Add icons to the links using the .nav-icon class
+	render() {
+		return (
+			<div className="container">
+				<aside className="main-sidebar sidebar-dark-primary elevation-4">
+					{/* Brand Logo */}
+					<Link to={'#'} className="brand-link">
+						<img
+							src="dist/img/Cda-Logo.jpeg"
+							alt="Cda Massy 2022"
+							className="brand-image img-circle elevation-3"
+							style={{ opacity: '.8' }}
+						/>
+						<span className="brand-text font-weight-light">Cda Massy 2022</span>
+					</Link>
+					{/* Sidebar */}
+					<div className="sidebar">
+						{/* Sidebar user panel (optional) */}
+						<div className="user-panel mt-3 pb-3 mb-3 d-flex">
+							<div className="image">
+								<img
+									src="dist/img/user2-160x160.jpg"
+									className="img-circle elevation-2"
+									alt="User Image"
+								/>
+							</div>
+							<div className="info">
+								<Link to={'#'} className="d-block">
+									Kamal knina
+								</Link>
+							</div>
+						</div>
+						{/* Sidebar Menu */}
+						<nav className="mt-2">
+							<ul
+								className="nav nav-pills nav-sidebar flex-column"
+								data-widget="treeview"
+								role="menu"
+								data-accordion="false"
+							>
+								{/* Add icons to the links using the .nav-icon class
            with font-awesome or any other icon font library */}
-          <li className="nav-item has-treeview menu-open">
-            <a href="#" className="nav-link active">
-              <i className="nav-icon fas fa-tachometer-alt" />
-              <p>
-                Canaux
-                <i className="right fas fa-angle-left" />
-              </p>
-            </a>
-            <ul className="nav nav-treeview">
-              <li className="nav-item">
-                <a href="./index.html" className="nav-link active">
-                  <i className="far fa-circle nav-icon" />
-                  <p>Canal 1</p>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="./index2.html" className="nav-link">
-                  <i className="far fa-circle nav-icon" />
-                  <p>Canal 2</p>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="./index3.html" className="nav-link">
-                  <i className="far fa-circle nav-icon" />
-                  <p>Canal 3</p>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="./index3.html" className="nav-link">
-                  <i className="far fa-circle nav-icon" />
-                  <p>Canal 4</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-       
-    
-        </ul>
-      </nav>
-      {/* /.sidebar-menu */}
-    </div>
-    {/* /.sidebar */}
-  </aside>
-</div>
-
-        )
-    }
+								<li className="nav-item has-treeview menu-open">
+									<Link to={'#'} className="nav-link active">
+										<p>
+											Canaux
+											<i className="right fas fa-angle-left" />
+										</p>
+									</Link>
+									<ul className="nav nav-treeview">
+										<li className="nav-item">
+											<Link to={'./index.html'} className="nav-link active">
+												<i className="far fa-circle nav-icon" />
+												<p>Canal 1</p>
+											</Link>
+										</li>
+										<li className="nav-item">
+											<Link to={'./index2.html'} className="nav-link">
+												<i className="far fa-circle nav-icon" />
+												<p>Canal 2</p>
+											</Link>
+										</li>
+										<li className="nav-item">
+											<Link to={'./index3.html'} className="nav-link">
+												<i className="far fa-circle nav-icon" />
+												<p>Canal 3</p>
+											</Link>
+										</li>
+										<li className="nav-item">
+											<Link to={'./index3.html'} className="nav-link">
+												<i className="far fa-circle nav-icon" />
+												<p>Canal 4</p>
+											</Link>
+										</li>
+									</ul>
+								</li>
+							</ul>
+						</nav>
+						{/* /.sidebar-menu */}
+					</div>
+					{/* /.sidebar */}
+				</aside>
+			</div>
+		);
+	}
 }
